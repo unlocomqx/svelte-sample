3e770ad32961bfe39bb1f74c454fb3f3ce23356b
unlocomqx <amxmodx.unloco@gmail.com>
2021-01-15 10:34:27 +0100

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  let count = 0;
+
+  $: double = count * 2;
+  $: quadruple = count * 4;
+  setInterval(() => {
+    count++;
+  }, 1000000);
+
+  setTimeout(() => {
+    count++;
+  }, 1000);
+
+  let counter = 0;
+  let double_counter = 0;
+  $: {
+    double_counter = counter * 2;
+  }
+
+</script>
+
+<p>
+  {count} * 2 = {double}
+</p>
+
+<p>
+  {count} * 4 = {quadruple}
+</p>
+
+<button on:click={() => counter++}>
+  Add
+</button>
+
+Result: {double_counter}
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const { reactivePreprocess } = require("svelte-reactive-preprocessor");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+const mode = process.env.NODE_ENV || "development";
+const prod = mode === "production";
+
+module.exports = {
+  entry: {
+    bundle: ["./src/main.js"]
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve("node_modules", "svelte")
+    },
+    extensions: [".mjs", ".js", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"]
+  },
+  output: {
+    path: __dirname + "/public",
+    filename: "[name].js",
+    chunkFilename: "[name].[id].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: "svelte-loader",
+          options: {
+            dev: !prod,
+            emitCss: true,
+            hotReload: true,
+            preprocess: reactivePreprocess()
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          /**
+           * MiniCssExtractPlugin doesn't support HMR.
+           * For developing, use 'style-loader' instead.
+           * */
+          prod ? MiniCssExtractPlugin.loader : "style-loader",
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  mode,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ],
+  devtool: prod ? false : "source-map",
+  devServer: {
+    host: '0.0.0.0',
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  }
+};
